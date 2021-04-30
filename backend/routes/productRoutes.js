42ea4fb55d740e97d6d97d6d26eccaaab5a115d5
vedatbaysal <vedatbaysall@gmail.com>
2021-04-30 23:04:38 +0300

    Fetchig Products From The Database

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import asyncHadler from 'express-async-handler'
+const router = express.Router()
+import Product from '../models/productModel.js'
+
+// @desc fetch all products
+// @route GET /api/products
+// @access Public
+router.get('/', asyncHadler(async (req, res) => {
+    const products = await Product.find({})
+    res.json(products)
+}))
+// @desc fetch single product
+// @route GET /api/products/:id
+// @access Public
+router.get('/:id', asyncHadler(async (req, res) => {
+    const product =  await Product.findById(req.params.id)
+    if(product) {
+        res.json(product)
+    } else {
+        res.status(404).json({message: 'Product not found'})
+    }
+} ))
+
+
+export default router
--- backend/seeder.js
+++ backend/seeder.js
@@ -19,11 +19,10 @@ const importData = async () => {
         await User.deleteMany()
 
         const createdUsers = await User.insertMany(users)
+        const adminUser = createdUsers[0]._id
         
-        const adminUsers = createdUsers[0]._id
-        
-        const sampleProducts = products.map(product => {
-            return {...product, user: adminUsers}
+        const sampleProducts = products.map((product) => {
+            return {...product, user: adminUser}
         })
 
         await Product.insertMany(sampleProducts)
