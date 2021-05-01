3eb1402b8bcf811b87fa8ff8263734eb5256935c
vedatbaysal <vedatbaysall@gmail.com>
2021-05-01 14:36:32 +0300

    Bringing Redux State Into HomeScreen - useDispatch & useSelector

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,20 +1,28 @@
-import React, {useState, useEffect} from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import {Row, Col} from 'react-bootstrap'
 import Product from '../components/Product'
-import axios from 'axios'
+import { listProducts } from '../actions/productActions.js'
+
 
 const HomeScreen = () => {
-    const [products, setProduct] = useState([])
+    const dispatch = useDispatch()
+
+    const productList = useSelector(state => state.productList)
+    const { loading, error, products } = productList
+
     useEffect(() => {
-        const fetchProduct = async () => {
-            const { data } = await axios.get('/api/products')
-            setProduct(data)
-        }
-        fetchProduct()
-    }, [])
+        dispatch(listProducts())
+    }, [dispatch])
+
     return (
         <>
             <h1>Latest Products</h1>
+            {loading ? (
+            <h2>Loading...</h2>
+            ) : error ? (
+            <h3> { error }</h3>
+            ) : (
             <Row>
                 {products.map(product => (
                     <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
@@ -22,6 +30,7 @@ const HomeScreen = () => {
                     </Col>
                 ))}
             </Row>
+            )}
         </>
     )
 }
