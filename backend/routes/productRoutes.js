de117e674be96965f99eb173b4dc477f6c753361
vedatbaysal <vedatbaysall@gmail.com>
2021-05-01 15:18:31 +0300

    Message & Loader Components

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,22 +6,27 @@ import Product from '../models/productModel.js'
 // @desc fetch all products
 // @route GET /api/products
 // @access Public
-router.get('/', asyncHadler(async (req, res) => {
+router.get(
+  '/',
+  asyncHadler(async (req, res) => {
     const products = await Product.find({})
     res.json(products)
-}))
+  })
+)
 // @desc fetch single product
 // @route GET /api/products/:id
 // @access Public
-router.get('/:id', asyncHadler(async (req, res) => {
-    const product =  await Product.findById(req.params.id)
-    if(product) {
-        res.json(product)
+router.get(
+  '/:id',
+  asyncHadler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+    if (product) {
+      res.json(product)
     } else {
-        res.status(404)
-        throw new Error('Product not found')
+      res.status(404)
+      throw new Error('Product not found')
     }
-} ))
-
+  })
+)
 
 export default router
